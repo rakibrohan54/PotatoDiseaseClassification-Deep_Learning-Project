5b94405feb7c67ec94c05636f0dc3b4922e283af
MD. RAKIBUL ISLAM <rakibrohan54@gmail.com>
2024-10-05 18:08:07 -0700

    data ingestion + prepase base model + Traning done

---
+++ src/cnnClassifier/components/model_training.py
@@ -0,0 +1,95 @@
+import os
+import urllib.request as request
+from zipfile import ZipFile
+import tensorflow as tf
+import time
+import math
+from pathlib import Path
+from cnnClassifier.entity.config_entity import TrainingConfig
+
+
+class Training:
+    def __init__(self, config: TrainingConfig):
+        self.config = config
+
+    def get_base_model(self):
+        self.model = tf.keras.models.load_model(
+            self.config.updated_base_model_path
+        )
+
+    def train_valid_generator(self):
+
+        datagenerator_kwargs = dict(
+            rescale=1./255,
+            validation_split=0.20
+        )
+
+        dataflow_kwargs = dict(
+            target_size=self.config.params_image_size[:-1],
+            batch_size=self.config.params_batch_size,
+            interpolation="bilinear"
+        )
+
+        # Validation data generator
+        valid_datagenerator = tf.keras.preprocessing.image.ImageDataGenerator(
+            **datagenerator_kwargs
+        )
+
+        self.valid_generator = valid_datagenerator.flow_from_directory(
+            directory=self.config.training_data,
+            subset="validation",
+            shuffle=False,
+            **dataflow_kwargs
+        )
+
+        # Train data generator with augmentation if enabled
+        if self.config.params_is_augmentation:
+            train_datagenerator = tf.keras.preprocessing.image.ImageDataGenerator(
+                rotation_range=40,
+                horizontal_flip=True,
+                width_shift_range=0.2,
+                height_shift_range=0.2,
+                shear_range=0.2,
+                zoom_range=0.2,
+                **datagenerator_kwargs
+            )
+        else:
+            train_datagenerator = valid_datagenerator
+
+        self.train_generator = train_datagenerator.flow_from_directory(
+            directory=self.config.training_data,
+            subset="training",
+            shuffle=True,
+            **dataflow_kwargs
+        )
+
+    @staticmethod
+    def save_model(path: Path, model: tf.keras.Model):
+        model.save(path)
+
+    def train(self):
+        # Compile the model before training
+        self.model.compile(
+            optimizer=tf.keras.optimizers.SGD(learning_rate=self.config.params_learning_rate),
+            loss=tf.keras.losses.CategoricalCrossentropy(),
+            metrics=["accuracy"]
+        )
+
+        # Define steps per epoch and validation steps
+        #self.steps_per_epoch = math.ceil(self.train_generator.samples / self.train_generator.batch_size)
+        #self.validation_steps = math.ceil(self.valid_generator.samples / self.valid_generator.batch_size)
+
+        # Train the model
+        self.model.fit(
+            self.train_generator,
+            epochs=self.config.params_epochs,
+            validation_data=self.valid_generator,
+            steps_per_epoch=None,
+            validation_steps=None
+        )
+
+        # Save the trained model
+        self.save_model(
+            path=self.config.trained_model_path,
+            model=self.model
+        )
