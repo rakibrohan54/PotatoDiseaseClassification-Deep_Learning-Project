cb75c9cfbeb7aedd295d367385d2d2c47a212c50
MD. RAKIBUL ISLAM <rakibrohan54@gmail.com>
2024-10-05 19:54:29 -0700

    evaluation with mlflow

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -3,7 +3,7 @@ import os
 from cnnClassifier.utils.common import read_yaml, create_directories,save_json
 from cnnClassifier.entity.config_entity import (DataIngestionConfig,
                                                 PrepareBaseModelConfig,
-                                                TrainingConfig)
+                                                TrainingConfig,EvaluationConfig)
 
 
 class ConfigurationManager:
@@ -79,8 +79,16 @@ class ConfigurationManager:
 
         return training_config
     
-
-    
+    def get_evaluation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model="artifacts/training/model.h5",
+            training_data="artifacts/data_ingestion/final_potato_diease_data",
+            mlflow_uri="https://dagshub.com/rakibrohan54/PotatoDiseaseClassification-Deep_Learning-Project.mlflow",
+            all_params=self.params,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE
+        )
+        return eval_config
 
 
 
--- src/cnnClassifier/entity/config_entity.py
+++ src/cnnClassifier/entity/config_entity.py
@@ -36,4 +36,11 @@ class TrainingConfig:
     params_learning_rate: float
 
 
-
+@dataclass(frozen=True)
+class EvaluationConfig:
+    path_of_model: Path
+    training_data: Path
+    all_params: dict
+    mlflow_uri: str
+    params_image_size: list
+    params_batch_size: int
